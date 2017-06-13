d482ddcb630c5be9c3c501b9487f338a0305c53d
Aum Pasawala <apasawala@aums-macbook-air.local>
2017-06-13 12:15:52 -0500

    Initial Commit

---
+++ minorityreport/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  minorityreport
+//
+//  Created by Aum Pasawala on 6/13/17.
+//  Copyright © 2017 Aum Pasawala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
