9ff75e5be7419d840e29d670c9789c2ed2c0fefa
Aum Pasawala <apasawala@aums-macbook-air.local>
2017-06-13 12:53:01 -0500

    initial commit

--- minorityreport/ViewController.swift
+++ minorityreport/ViewController.swift
@@ -8,18 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var futureLabel: UILabel!
+    @IBOutlet weak var preCogsLabel: UILabel!
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
+      
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onDrag(_ sender: UIPanGestureRecognizer)
+    {
+        print("Drag")
+        
+        let point = sender.location(in: view)
+        print(point.x , point.y)
+        futureLabel.center = point
+        
+        if sender.state == .ended
+        {
+            print("end")
+            UIView.animate(withDuration: 0.75, animations:
+                {
+                    self.futureLabel.center = self.view.center
+            })
     }
-
-
-}
+    }
+            }
 
